23909e1cbb457b44589013ea98e330ec9f2bf18e
SriVidyaBehara <srvidya16@gmail.com>
2019-11-13 14:33:43 +0000

    added git ignore file

---
+++ src/main/java/qamasters/pages/ProductPage.java
@@ -0,0 +1,45 @@
+package qamasters.pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import qamasters.Utils;
+
+public class ProductPage extends BasePage {
+    @FindBy(linkText="Faded Short Sleeve T-shirts")
+    public WebElement fadedTshirt;
+    @FindBy(css="#quantity_wanted")
+    public WebElement quantityField;
+    @FindBy(css="#group_1")
+    public WebElement sizeList;
+    @FindBy(name="Blue")
+    public WebElement color;
+    @FindBy(name="Submit")
+    public WebElement addToCartButton;
+    @FindBy(css=".layer_cart_product")
+    public WebElement cartPopUp;
+    public ProductPage(){
+        PageFactory.initElements(driver,this);
+
+    }
+    public void clickfadedTshirt(){
+        fadedTshirt.click();
+    }
+    public void enterQuantity(String quantity){
+        quantityField.sendKeys("quantity");
+
+    }
+    public void selectSize(String size){
+        Utils.selectFromList(sizeList,size);
+    }
+    public void selectColor(){
+        color.click();
+    }
+    public void addToCart(){
+        addToCartButton.click();
+    }
+    public void itemSuccessfullyAddedToCart(){
+        Assert.assertTrue(cartPopUp.isDisplayed());
+    }
+}
